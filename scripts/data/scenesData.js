ff18a8c54e153ef47cc6e94c48627557c9f47f65
Leslie Culcay <116099796+Lesculcay04@users.noreply.github.com>
2023-06-21 17:18:12 -0500

    Nivel 9 Traicion

--- scripts/data/scenesData.js
+++ scripts/data/scenesData.js
@@ -185,6 +185,20 @@ export const scenes = {
         ],
         styles: "sceneExample.css"
     },
+    traicion:{
+        title:'Traicion',
+        description:{
+            context: 'Al llegar al último círculo Dantes se encuentra ante un lugar conocido como el "Traidores". Este círculo está dividido en cuatro fosas concéntricas, cada una destinada a diferentes tipos de traición. En este noveno círculo, se encuentran diferentes formas de traición, como la traición política, la traición a la familia, la traición a los huéspedes y la traición a los benefactores. Cada fosa tiene su propio castigo específico para cada tipo de traición',
+            text: '¿Cuáles son las diferentes formas de traición que Dante encuentra a lo largo de su viaje en la obra?',
+            time: 0.5
+        },
+        imgUrl:"https://upload.wikimedia.org/wikipedia/commons/thu…re_Inferno34.jpg/300px-Gustave_Dore_Inferno34.jpg",
+        decisions:[
+            {description: 'Dante encuentra diferentes formas de traición, que incluyen la traición política, la traición familiar y la traición religiosa', next:"start"},
+            {description: 'Dante solo encuentra una forma de traición', type: decisionType.decreaseLife, appear: false}
+        ],
+        styles: "sceneExample.css"
+    },
     gameover: {
         description: {
             text:"GAME OVER"
