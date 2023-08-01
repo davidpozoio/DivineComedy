627d2c221a4eda4717d89b26a8d819700f58f5bf
carlosduti70 <cmzuniga@sudamericano.edu.ec>
2023-08-01 08:55:11 -0500

    creditos finales

---
+++ scripts/data/levels/final.js
@@ -0,0 +1,18 @@
+export const final = {
+    description: {
+        context: `Tras haber superado los nueve círculos del infierno, Dantes finalmente alcanzó la salida y se encontró frente a las imponentes puertas del Purgatorio. Allí, pudo vislumbrar su próximo destino en esta travesía espiritual. A lo lejos, las puertas se alzaban majestuosas, anunciando un nuevo capítulo en su viaje hacia la redención y la purificación.
+        Virgilio, su guía hasta ese momento, miró a Dantes con una expresión llena de afecto y sabiduría. "Este es el fin de mi camino", le dijo con voz serena, "pero el tuyo apenas comienza.
+        CONTINUARÁ...`
+        ,
+        time: 60
+    },
+    imgUrl: "../assets/img/purgatorio.jpg",
+    decisions: [
+      {
+        description:
+          "Volver a jugar",
+        next: "start",
+      },
+    ],
+    styles: "finalScene.css",
+}
